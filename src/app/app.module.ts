a5c29deccb074a3a437d967d760b4a0c94c81053
edgar031824 <edgar031824@hotmail.com>
2018-09-19 14:42:22 -0500

    se añaden proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//rutas
+import { app_routing } from './app.routes';
+//servicios
+import { HeroesService } from './services/heroes.service';
+
+//componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { SearchHeroeComponent } from './components/search-heroe/search-heroe.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    SearchHeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
+
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
